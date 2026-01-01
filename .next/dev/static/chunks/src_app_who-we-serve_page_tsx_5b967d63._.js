99790d64de94b1a93697ac83e90a02991c7da3c8
Deon Menezes <deonmenezescodes@gmail.com>
2026-01-01 16:21:48 +0530

    hh

---
+++ .next/dev/static/chunks/src_app_who-we-serve_page_tsx_5b967d63._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_who-we-serve_page_tsx_a7be88ca._.js"
+],
+    source: "dynamic"
+});
